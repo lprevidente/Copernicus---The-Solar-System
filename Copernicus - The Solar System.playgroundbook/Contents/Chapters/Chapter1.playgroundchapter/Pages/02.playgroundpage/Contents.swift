f7819637b0ba8e81827595cc97cc9cf4e245cba0
Luigi Previdente <luigi.previdente@studenti.unina2.it>
2018-03-27 22:40:09 +0200

    Some changes on images and funcs

---
+++ Copernicus - The Solar System.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/02.playgroundpage/Contents.swift
@@ -0,0 +1,57 @@
+/*:
+ # **Start from the basis**
+ 
+ Venturing in the `On the Revolutions of the Heavenly Spheres` (1543) we can found out how the solar system referred is `Heliocentric`: the centre of everything is the Sun, it is fixed and it's 'near' the centre of the universe, but *why near? *.
+ 
+ So start to build the solar system:
+ 
+ 1. You need to **Create the Sun**. *It will be in front of you*.
+ 2. What is a sun without anything? We know is mainly composed of Hydrogen and Helium, so put a **Texture** on it.
+ 3. It isn't totally fixed, it rotates around its own axis. You have two ways to put it into motion. `Try to discover what are`.
+ */
+//#-hidden-code
+import PlaygroundSupport
+import UIKit
+import ARKit
+
+let page = PlaygroundPage.current
+page.needsIndefiniteExecution = true
+let proxy = page.liveView as? PlaygroundRemoteLiveViewProxy
+
+func createSun(radius: CGFloat, position: SCNVector3) {
+    proxy?.send(MessageFromContentsToLiveView.createSun(radius: radius, position: position).playgroundValue)
+}
+
+func setTextureToSun() {
+    proxy?.send(MessageFromContentsToLiveView.setTextureToSun.playgroundValue)
+}
+
+func setSpeedRotationToSun(speedRotation: Int) {
+    proxy?.send(MessageFromContentsToLiveView.setSpeedRotationToSun(speedRotation: speedRotation).playgroundValue)
+}
+
+// Handle messages from the live view.
+class Listener: PlaygroundRemoteLiveViewProxyDelegate {
+    func remoteLiveViewProxy(_ remoteLiveViewProxy: PlaygroundRemoteLiveViewProxy,
+                             received message: PlaygroundValue) {
+        guard let liveViewMessage = MessageFromLiveViewToContents(playgroundValue: message) else { return }
+        switch liveViewMessage {
+        case .succeeded:
+            page.assessmentStatus = .pass(message: "Now we have a beautiful sun! [Next](@next) ")
+        default:
+            break
+        }
+    }
+    func remoteLiveViewProxyConnectionClosed(_ remoteLiveViewProxy: PlaygroundRemoteLiveViewProxy) { }
+}
+
+let listener = Listener()
+proxy?.delegate = listener
+//#-code-completion(everything, hide)
+//#-code-completion(identifier, show, setSpeedRotationToSun(speedRotation:), setTextureToSun())
+//#-code-completion(identifier, show)
+//#-end-hidden-code
+createSun(radius:/*#-editable-code*/0.35/*#-end-editable-code*/, position: SCNVector3(/*#-editable-code 0*/0/*#-end-editable-code*/,/*#-editable-code */0/*#-end-editable-code*/,/*#-editable-code -1*/-2/*#-end-editable-code*/))
+//#-editable-code
+
+//#-end-editable-code
---
+++ Copernicus - The Solar System.playgroundbook/Contents/Sources/PlaygroundMessage.swift
@@ -0,0 +1,56 @@
+import Foundation
+import PlaygroundSupport
+
+// Defines a type that represents a specific type of message
+public protocol PlaygroundMessageType {
+    init?(rawValue: String)
+    var rawValue: String { get }
+}
+
+// Protocol that define the type of Playground Message
+public protocol PlaygroundMessage {
+    associatedtype T: PlaygroundMessageType
+    
+    var messageType: T { get }
+    
+    init?(messageType: T, parametersEncoded: Data?)
+    
+    func encodeParameters() -> Data?
+}
+
+// MARK: Extension
+// Add encoding func
+public extension PlaygroundMessage {
+    
+    /// An initializer to create a 'PlaygroundMessage' from a 'PlaygroundValue' that contains an encoded type and optional payload
+    public init?(playgroundValue: PlaygroundValue) {
+        // Extract the required values from the supplied PlaygroundValue
+        guard case let .array(values) = playgroundValue, !values.isEmpty else { fatalError("Expected an array of values") }
+        guard case let .string(rawType) = values[0] else { fatalError("Unexpected Playground value type") }
+        
+        // Check if this is a supported type
+        guard let messageType = T(rawValue: rawType) else { return nil }
+        
+        // Extract any encoded payload.
+        let parametersEncoded: Data?
+        if values.count > 1, case let .data(parameters) = values[1] {
+            parametersEncoded = parameters
+        }
+        else {
+            parametersEncoded = nil
+        }
+        
+        self.init(messageType: messageType, parametersEncoded: parametersEncoded)
+    }
+    
+    // A 'PlaygroundValue' representation of the message
+    // Playground Value = [ nameFunction, data(parameters) ]
+    public var playgroundValue: PlaygroundValue {
+        if let parametersEncoded = self.encodeParameters() {
+            return .array([.string(messageType.rawValue), .data(parametersEncoded)])
+        }
+        else {
+            return .array([.string(messageType.rawValue)])
+        }
+    }
+}
